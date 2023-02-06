67da87da51ccbb748693f1fc4921c4d77644f6d8
Daniel  van Niekerk <daniel@skynamo.com>
2023-02-06 17:02:47 +0200

    Updated classes exported from package

--- skynamo/__init__.py
+++ skynamo/__init__.py
@@ -1,6 +1,7 @@
-
-from .getters import getCreditRequests, getCustomers, getOrders, getProducts, getQuotes, getFormResults,getInvoices,getStockLevels,getUsers
-from .instanceDataClasses import updateInstanceDataClasses
-from .outputters import writeListOfObjectsToCsvWithObjectPropertiesAsColumnNames,sendEmailUsingGmailCredentialsWithFilesAttached
-from .skynamoDataClasses.StockLevel import getStockLevelPutUsingProductCodeAndOrderUnitId,getStockLevelPutUsingProductId,getStockLevelPutUsingProductCodeAndOrderUnitName
-from .SkynamoAPI import makeWrites
+from skynamo_data.Writer import Writer
+from skynamo_data.Reader import Reader
+from .models.Address import Address
+from .models.OrderUnit import OrderUnit
+from .models.InvoiceItem import InvoiceItem
+from .models.Location import Location
+from .models.LineItem import LineItem
